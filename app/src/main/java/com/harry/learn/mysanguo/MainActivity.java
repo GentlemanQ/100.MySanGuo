b38a587514ea21147e0061b833a6b18fef3a5525
GentlemanQ <harryqxb@sina.com>
2018-03-30 22:18:51 +0800

    载入游戏列表
    新增一个载入游戏列表的Activity。

--- app/src/main/java/com/harry/learn/mysanguo/MainActivity.java
+++ app/src/main/java/com/harry/learn/mysanguo/MainActivity.java
@@ -1,13 +1,45 @@
 package com.harry.learn.mysanguo;
 
+import android.content.Intent;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnLoadGame, btnnewGame, btnTeaching, btnSetting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLoadGame = findViewById(R.id.btn_load_game);
+        btnnewGame = findViewById(R.id.btn_new_game);
+        btnTeaching = findViewById(R.id.btn_teaching);
+        btnSetting = findViewById(R.id.btn_setting);
+
+        btnLoadGame.setOnClickListener(this);
+        btnnewGame.setOnClickListener(this);
+        btnTeaching.setOnClickListener(this);
+        btnSetting.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.btn_load_game:
+                Intent i = new Intent(this, loadGameListActivity.class);
+                startActivity(i);
+                break;
+            case R.id.btn_new_game:
+                break;
+            case R.id.btn_teaching:
+                break;
+            case R.id.btn_setting:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/harry/learn/mysanguo/loadGameListActivity.java
@@ -0,0 +1,18 @@
+package com.harry.learn.mysanguo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Urbane on 2018/3/30.
+ */
+
+public class loadGameListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.load_game_layout);
+    }
+}
